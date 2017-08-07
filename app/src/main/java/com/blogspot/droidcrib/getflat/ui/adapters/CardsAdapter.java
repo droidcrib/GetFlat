68f7469d5d3ee98dd43032efe71276cac05ab3ca
Andrey Bulanov <droidcrib@gmail.com>
2017-08-07 10:38:14 +0300

    Adapter seems works correct

--- app/src/main/java/com/blogspot/droidcrib/getflat/ui/adapters/CardsAdapter.java
+++ app/src/main/java/com/blogspot/droidcrib/getflat/ui/adapters/CardsAdapter.java
@@ -34,18 +34,43 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.CardViewHold
     @Override
     public void onBindViewHolder(CardViewHolder holder, int position) {
         Card card = cardList.get(position);
-        holder.address.setText(card.geo.address.getStreetOrBuilding() + " " +card.geo.address.house);
-        holder.district.setText(card.geo.district.name);
-        holder.microdistrict.setText("card.geo.microdistrict.name");
-        holder.price.setText(card.price);
-        holder.rooms.setText("4 rooms");
-        holder.project.setText("good project");
-        holder.meters.setText("150 meters");
-        holder.year.setText("2017");
-        holder.floor.setText("13-floor");
-        holder.material.setText("square blocks");
-        holder.descriprion.setText(card.description.text);
-
+        if (card.geo.address != null && card.geo.address.streetOrBuilding != null) {
+            holder.street.setText(card.geo.address.streetOrBuilding);
+        }
+        if (card.geo.address != null
+                && card.geo.address.house != null) {
+            holder.number.setText(card.geo.address.house);
+        }
+        if (card.geo.district != null && card.geo.district.name != null) {
+            holder.district.setText(card.geo.district.name);
+        }
+        if (card.geo.microdistrict != null && card.geo.microdistrict.name != null) {
+            holder.microdistrict.setText(card.geo.microdistrict.name);
+        }
+        if (card.price != null) {
+            holder.price.setText(card.price);
+        }
+        if (card.realtyFeatures != null && card.realtyFeatures.size() >= 1) {
+            holder.rooms.setText(card.realtyFeatures.get(0).value);
+        }
+        if (card.houseFeatures != null && card.houseFeatures.size() >= 1) {
+            holder.project.setText(card.houseFeatures.get(0).value);
+        }
+        if (card.realtyFeatures != null && card.realtyFeatures.size() >= 2) {
+            holder.meters.setText(card.realtyFeatures.get(1).value);
+        }
+        if (card.houseFeatures != null && card.houseFeatures.size() >= 2) {
+            holder.year.setText(card.houseFeatures.get(1).value);
+        }
+        if (card.realtyFeatures != null && card.realtyFeatures.size() >= 3) {
+            holder.floor.setText(card.realtyFeatures.get(2).value);
+        }
+        if (card.houseFeatures != null && card.houseFeatures.size() >= 3) {
+            holder.material.setText(card.houseFeatures.get(2).value);
+        }
+        if (card.description != null) {
+            holder.descriprion.setText(card.description.text);
+        }
     }
 
     @Override
@@ -54,12 +79,13 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.CardViewHold
     }
 
     public class CardViewHolder extends RecyclerView.ViewHolder {
-        public TextView address, district, microdistrict, price, rooms, project, meters, year,
+        public TextView street, number, district, microdistrict, price, rooms, project, meters, year,
                 floor, material, descriprion;
 
         public CardViewHolder(View view) {
             super(view);
-            address = (TextView) view.findViewById(R.id.id_list_row_address);
+            street = (TextView) view.findViewById(R.id.id_list_row_street);
+            number = (TextView) view.findViewById(R.id.id_list_row_number);
             district = (TextView) view.findViewById(R.id.id_list_row_district);
             microdistrict = (TextView) view.findViewById(R.id.id_list_row_microdistrict);
             price = (TextView) view.findViewById(R.id.id_list_row_price);
