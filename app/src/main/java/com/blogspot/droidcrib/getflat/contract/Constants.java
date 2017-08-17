f7cc0afdfdfbe057a59b1298f96d596d24cd3136
Andrey Bulanov <droidcrib@gmail.com>
2017-08-17 09:09:37 +0300

    parameter selection views setup ongoing

---
+++ app/src/main/java/com/blogspot/droidcrib/getflat/contract/Constants.java
@@ -0,0 +1,23 @@
+package com.blogspot.droidcrib.getflat.contract;
+
+/**
+ * Created by BulanovA on 17.08.2017.
+ */
+
+public class Constants {
+
+    public static final String SHARED_PREFS = "com.blogspot.droidcrib.getflat.contract.SHARED_PREFS";
+
+    public static final String PREFS_CITY = "com.blogspot.droidcrib.getflat.contract.PREFS_CITY";
+    public static final String PREFS_DISTRICT = "com.blogspot.droidcrib.getflat.contract.PREFS_DISTRICT";
+    public static final String PREFS_ROOMS = "com.blogspot.droidcrib.getflat.contract.PREFS_ROOMS";
+    public static final String PREFS_AREA = "com.blogspot.droidcrib.getflat.contract.PREFS_AREA";
+    public static final String PREFS_PRICE = "com.blogspot.droidcrib.getflat.contract.PREFS_PRICE";
+    public static final String PREFS_RADIO_DISTRICT = "com.blogspot.droidcrib.getflat.contract.PREFS_RADIO_DISTRICT";
+    public static final String PREFS_RADIO_METRO = "com.blogspot.droidcrib.getflat.contract.PREFS_RADIO_METRO";
+    public static final String PREFS_CHECKBOX_BUILDING = "com.blogspot.droidcrib.getflat.contract.PREFS_CHECKBOX_BUILDING";
+    public static final String PREFS_CHECKBOX_SUBWAY = "com.blogspot.droidcrib.getflat.contract.PREFS_CHECKBOX_SUBWAY";
+    public static final String PREFS_CHECKBOX_WITH_PHOTO = "com.blogspot.droidcrib.getflat.contract.PREFS_CHECKBOX_WITH_PHOTO";
+    public static final String PREFS_CHECKBOX_NO_FEE = "com.blogspot.droidcrib.getflat.contract.PREFS_CHECKBOX_NO_FEE";
+    public static final String PREFS_CHECKBOX_NO_BROKERS = "com.blogspot.droidcrib.getflat.contract.PREFS_CHECKBOX_NO_BROKERS";
+}
--- app/src/main/java/com/blogspot/droidcrib/getflat/ui/activities/SelectionActivity.java
+++ app/src/main/java/com/blogspot/droidcrib/getflat/ui/activities/SelectionActivity.java
@@ -1,10 +1,13 @@
 package com.blogspot.droidcrib.getflat.ui.activities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -19,6 +22,15 @@ import com.blogspot.droidcrib.getflat.model.parameters.PriceParam;
 import com.blogspot.droidcrib.getflat.model.parameters.RoomsParam;
 
 import java.util.List;
+
+import static com.blogspot.droidcrib.getflat.contract.Constants.PREFS_CHECKBOX_BUILDING;
+import static com.blogspot.droidcrib.getflat.contract.Constants.PREFS_CHECKBOX_NO_BROKERS;
+import static com.blogspot.droidcrib.getflat.contract.Constants.PREFS_CHECKBOX_NO_FEE;
+import static com.blogspot.droidcrib.getflat.contract.Constants.PREFS_CHECKBOX_SUBWAY;
+import static com.blogspot.droidcrib.getflat.contract.Constants.PREFS_CHECKBOX_WITH_PHOTO;
+import static com.blogspot.droidcrib.getflat.contract.Constants.PREFS_RADIO_DISTRICT;
+import static com.blogspot.droidcrib.getflat.contract.Constants.PREFS_RADIO_METRO;
+import static com.blogspot.droidcrib.getflat.contract.Constants.SHARED_PREFS;
 
 /**
  * Created by BulanovA on 07.08.2017.
@@ -35,11 +47,13 @@ public class SelectionActivity extends AppCompatActivity {
     private CheckBox mCheckBoxWithPhoto;
     private CheckBox mCheckBoxNoFee;
     private CheckBox mCheckBoxNoBrokers;
+    private SharedPreferences mPrefs;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
+        mPrefs = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
 
         Spinner spinnerCity = (Spinner) findViewById(R.id.spinner_city);
         Spinner spinnerDistrict = (Spinner) findViewById(R.id.spinner_district);
@@ -53,7 +67,6 @@ public class SelectionActivity extends AppCompatActivity {
         mCheckBoxWithPhoto = (CheckBox) findViewById(R.id.checkbox_with_photo);
         mCheckBoxNoFee = (CheckBox) findViewById(R.id.checkbox_no_fee);
         mCheckBoxNoBrokers = (CheckBox) findViewById(R.id.checkbox_no_brokers);
-
 
         // Setup spinners
         List<AreaParam> areas = AreaParam.queryAll();
@@ -73,6 +86,17 @@ public class SelectionActivity extends AppCompatActivity {
         spinnerRooms.setAdapter(roomAdapter);
         spinnerPrice.setAdapter(priceAdapter);
         spinnerCity.setAdapter(cityAdapter);
+        spinnerArea.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
 
         // Setup radiobuttons
@@ -98,12 +122,15 @@ public class SelectionActivity extends AppCompatActivity {
                 case R.id.radiobutton_district:
                     Log.d(TAG, "onClick: DISTRICT radiobutton selected " + mRadioButtonDistrict.isChecked());
                     Log.d(TAG, "onClick: METRO radiobutton selected " + mRadioButtonMetro.isChecked());
+                    mPrefs.edit().putBoolean(PREFS_RADIO_DISTRICT, mRadioButtonDistrict.isChecked()).apply();
+                    mPrefs.edit().putBoolean(PREFS_RADIO_METRO, mRadioButtonMetro.isChecked()).apply();
 
                     break;
                 case R.id.radiobutton_metro:
                     Log.d(TAG, "onClick: DISTRICT radiobutton selected " + mRadioButtonDistrict.isChecked());
                     Log.d(TAG, "onClick: METRO radiobutton selected " + mRadioButtonMetro.isChecked());
-
+                    mPrefs.edit().putBoolean(PREFS_RADIO_DISTRICT, mRadioButtonDistrict.isChecked()).apply();
+                    mPrefs.edit().putBoolean(PREFS_RADIO_METRO, mRadioButtonMetro.isChecked()).apply();
                     break;
 
                 default:
@@ -119,18 +146,23 @@ public class SelectionActivity extends AppCompatActivity {
             switch (checkBox.getId()) {
                 case R.id.checkbox_new_building:
                     Log.d(TAG, "onCheckedChanged: new building " + mCheckBoxNewBuilding.isChecked());
+                    mPrefs.edit().putBoolean(PREFS_CHECKBOX_BUILDING, mCheckBoxNewBuilding.isChecked()).apply();
                     break;
                 case R.id.checkbox_near_subway:
                     Log.d(TAG, "onCheckedChanged: near subway " + mCheckBoxNearSubway.isChecked());
+                    mPrefs.edit().putBoolean(PREFS_CHECKBOX_SUBWAY, mCheckBoxNearSubway.isChecked()).apply();
                     break;
                 case R.id.checkbox_with_photo:
                     Log.d(TAG, "onCheckedChanged: with photo " + mCheckBoxWithPhoto.isChecked());
+                    mPrefs.edit().putBoolean(PREFS_CHECKBOX_WITH_PHOTO, mCheckBoxWithPhoto.isChecked()).apply();
                     break;
                 case R.id.checkbox_no_fee:
                     Log.d(TAG, "onCheckedChanged: no feee " + mCheckBoxNoFee.isChecked());
+                    mPrefs.edit().putBoolean(PREFS_CHECKBOX_NO_FEE, mCheckBoxNoFee.isChecked()).apply();
                     break;
                 case R.id.checkbox_no_brokers:
                     Log.d(TAG, "onCheckedChanged: no brockers " + mCheckBoxNoBrokers.isChecked());
+                    mPrefs.edit().putBoolean(PREFS_CHECKBOX_NO_BROKERS, mCheckBoxNoBrokers.isChecked()).apply();
                     break;
 
 
@@ -140,5 +172,38 @@ public class SelectionActivity extends AppCompatActivity {
         }
     };
 
+    AdapterView.OnItemSelectedListener spinnerItemSelectedListener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            Spinner spinner = (Spinner) view;
+            switch (spinner.getId()) {
+                case R.id.:
 
-}
+                    break;
+                case R.id.:
+
+                    break;
+                case R.id.:
+
+                    break;
+                case R.id.:
+
+                    break;
+                case R.id.:
+
+                    break;
+
+                default:
+                    break;
+            }
+
+            @Override
+            public void onNothingSelected (AdapterView < ? > adapterView){
+
+            }
+        }
+
+        ;
+
+
+    }
